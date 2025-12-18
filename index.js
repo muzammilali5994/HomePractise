14d2ebac9df483a7f0669e69ae919fad83816f12
muzammilali5994 <muzammilali5994@outlook.com>
2025-12-18 20:26:21 +0500

    updated

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 
-let a= [1,2,3,4,5];
+// let a= [1,2,3,4,5];
 
-let reverse=[];
+// let reverse=[];
 
-for(let i= a.length-1; i>=0; i--){
+// for(let i= a.length-1; i>=0; i--){
  
-    reverse.push(a[i]);
+//     reverse.push(a[i]);
  
-}
+// }
 
-console.log(reverse);
+// console.log(reverse);
+
+// let a= [1,2,3,4,5];
+// let min = a[0];
+// for(let i =0; i<a.length; i++){
+//   if(a[i]<min){
+//     min = a[i];
+    
+//   }
+// }
+// console.log(min);
+
+// function sortStack(stack) {
+//   let temp = [];
+
+//   while (stack.length) {
+//     let curr = stack.pop();
+//     while (temp.length && temp[temp.length - 1] > curr) {
+//       stack.push(temp.pop());
+//     }
+//     temp.push(curr);
+//   }
+//   return temp;
+// }
+
+// console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]
+
+
+// function sortStack(stack) {
+//   return stack.sort();
+// }
+
+// console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]

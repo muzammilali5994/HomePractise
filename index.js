1a75e2a638678a5a37744b1ce94024597df7d845
muzammilali5994 <muzammilali5994@outlook.com>
2026-01-03 13:07:12 +0500

    event complete

--- index.js
+++ index.js
@@ -1,426 +1,468 @@
 
-let a= [1,2,3,4,5];
+// let a= [1,2,3,4,5];
 
-let reverse=[];
+// let reverse=[];
 
-for(let i= a.length-1; i>=0; i--){
+// for(let i= a.length-1; i>=0; i--){
  
-    reverse.push(a[i]);
+//     reverse.push(a[i]);
  
-}
+// }
 
-console.log(reverse);
+// console.log(reverse);
 
-let a= [1,2,3,4,5];
-let min = a[0];
-for(let i =0; i<a.length; i++){
-  if(a[i]<min){
-    min = a[i];
+// let a= [1,2,3,4,5];
+// let min = a[0];
+// for(let i =0; i<a.length; i++){
+//   if(a[i]<min){
+//     min = a[i];
     
-  }
-}
-console.log(min);
+//   }
+// }
+// console.log(min);
 
-function sortStack(stack) {
-  let temp = [];
+// function sortStack(stack) {
+//   let temp = [];
 
-  while (stack.length) {
-    let curr = stack.pop();
-    while (temp.length && temp[temp.length - 1] > curr) {
-      stack.push(temp.pop());
-    }
-    temp.push(curr);
-  }
-  return temp;
-}
+//   while (stack.length) {
+//     let curr = stack.pop();
+//     while (temp.length && temp[temp.length - 1] > curr) {
+//       stack.push(temp.pop());
+//     }
+//     temp.push(curr);
+//   }
+//   return temp;
+// }
 
-console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]
-
-
-function sortStack(stack) {
-  return stack.sort();
-}
-
-console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]
-
-function sortStack(stack) {
-  let temp = [];
-
-  while (stack.length) {
-    let curr = stack.pop();
-    while (temp.length && temp[temp.length - 1] > curr) {
-      stack.push(temp.pop());
-    }
-    temp.push(curr);
-  }
-  return temp;
-}
-
-console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]
-
-convert string to arry 
-
-let str = "hello world";
-let arr= str.split("");
-console.log(arr);
-
-let str = "hello world";
-let arr= Array.from(str);
-console.log(arr);
-
-let str= "hello world";
-let arr=[];
-for(let i =0; i<str.length; i++){
-  arr[i]=str[i];
-
-}
-console.log(arr);
-
-let arr = ["JS", "is", "easy"];
-let str= arr.join(" ");
-console.log(str);
-
-reverse string
-let str = "javascript";
-
-let result= str.split("");
-
-result = result.reverse();
-result = result.join("");
-
-console.log('"'+result+'"');
-
-let str = "javascript";
-let result = "";
-for(let i = str.length-1; i>=0; i--){
-    result += str[i];
-}
-console.log(result);
-
-check palandrome
-
-let str = "madam";
-if(str === str.split("").reverse().join("")){
-  console.log("palandrome");
-}
-
-let str = "madam";
-for(let i =0; i<=str.length; i++){
-  for(let j = str.length-1; j>=0; j--){
-    if(str[i] === str[j]){
-      console.log("palandrome");
-    }
-  }
-}
-let str = "madam";
-let isPalandrome = true;
-for(let i =0; i<=str.length; i++){
-  if(str[i] !== str[str.length-1-i]){
-    isPalandrome = false;
-    break;
-  }
-}
-if(isPalandrome){
-  console.log("palandrome");
-} else{
-  console.log("not palandrome");
-}
-
-let str = "aabbccdeeFF";
-let empt = "";
-
-for (let i = 0; i < str.length; i++) {
-  let found = false;
-
-  for (let j = 0; j < empt.length; j++) {
-    if (empt[j] === str[i]) {
-      found = true;
-      break;
-    }
-  }
-
-  if (!found) {
-    empt += str[i];
-  }
-}
-
-console.log(empt); // "abcd"
-
-let sentence = "hello world";
-let words = sentence.split(" ");
-let result = [];
-
-for (let i = 0; i < words.length; i++) {
-  let word = words[i];
-  let capitalWord = word[0].toUpperCase()+word.slice(1);
-  result.push(capitalWord);
-}
-
-console.log(result); // "Hello World"
-
-let str1 = "listen";
-let str2 = "silent";
-
-if (str1.length !== str2.length) {
-  console.log(false);
-} else {
-  let isAnagram = true;
-
-  for (let i = 0; i < str2.length; i++) {
-    if (!str1.includes(str2[i])) {
-      isAnagram = false;
-      break;
-    }
-  }
-
-  console.log(isAnagram); // true
-}
+// console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]
 
 
-let num=15,fact=1;
-for(let i=1;i<=num;i++) 
-  fact*=i;
-console.log(fact);
+// function sortStack(stack) {
+//   return stack.sort();
+// }
 
-let a =0;
-let b =1;
-let n = 10;
+// console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]
 
-for(let i=1;i<=n;i++){
- [a+b]= [b,a+b]
+// function sortStack(stack) {
+//   let temp = [];
 
-}
+//   while (stack.length) {
+//     let curr = stack.pop();
+//     while (temp.length && temp[temp.length - 1] > curr) {
+//       stack.push(temp.pop());
+//     }
+//     temp.push(curr);
+//   }
+//   return temp;
+// }
 
-shift zero to end
+// console.log(sortStack([4, 1, 3, 2])); // [1,2,3,4]
+
+// convert string to arry 
+
+// let str = "hello world";
+// let arr= str.split("");
+// console.log(arr);
+
+// let str = "hello world";
+// let arr= Array.from(str);
+// console.log(arr);
+
+// let str= "hello world";
+// let arr=[];
+// for(let i =0; i<str.length; i++){
+//   arr[i]=str[i];
+
+// }
+// console.log(arr);
+
+// let arr = ["JS", "is", "easy"];
+// let str= arr.join(" ");
+// console.log(str);
+
+// reverse string
+// let str = "javascript";
+
+// let result= str.split("");
+
+// result = result.reverse();
+// result = result.join("");
+
+// console.log('"'+result+'"');
+
+// let str = "javascript";
+// let result = "";
+// for(let i = str.length-1; i>=0; i--){
+//     result += str[i];
+// }
+// console.log(result);
+
+// check palandrome
+
+// let str = "madam";
+// if(str === str.split("").reverse().join("")){
+//   console.log("palandrome");
+// }
+
+// let str = "madam";
+// for(let i =0; i<=str.length; i++){
+//   for(let j = str.length-1; j>=0; j--){
+//     if(str[i] === str[j]){
+//       console.log("palandrome");
+//     }
+//   }
+// }
+// let str = "madam";
+// let isPalandrome = true;
+// for(let i =0; i<=str.length; i++){
+//   if(str[i] !== str[str.length-1-i]){
+//     isPalandrome = false;
+//     break;
+//   }
+// }
+// if(isPalandrome){
+//   console.log("palandrome");
+// } else{
+//   console.log("not palandrome");
+// }
+
+// let str = "aabbccdeeFF";
+// let empt = "";
+
+// for (let i = 0; i < str.length; i++) {
+//   let found = false;
+
+//   for (let j = 0; j < empt.length; j++) {
+//     if (empt[j] === str[i]) {
+//       found = true;
+//       break;
+//     }
+//   }
+
+//   if (!found) {
+//     empt += str[i];
+//   }
+// }
+
+// console.log(empt); // "abcd"
+
+// let sentence = "hello world";
+// let words = sentence.split(" ");
+// let result = [];
+
+// for (let i = 0; i < words.length; i++) {
+//   let word = words[i];
+//   let capitalWord = word[0].toUpperCase()+word.slice(1);
+//   result.push(capitalWord);
+// }
+
+// console.log(result); // "Hello World"
+
+// let str1 = "listen";
+// let str2 = "silent";
+
+// if (str1.length !== str2.length) {
+//   console.log(false);
+// } else {
+//   let isAnagram = true;
+
+//   for (let i = 0; i < str2.length; i++) {
+//     if (!str1.includes(str2[i])) {
+//       isAnagram = false;
+//       break;
+//     }
+//   }
+
+//   console.log(isAnagram); // true
+// }
 
 
-let a = [1,2,3,0,11,5,0,5];
-let result = [];
-let alength = 0;
-for(let i=0; i<a.length;i++){
-  if(a[i]!== 0){
-    result.push(a[i]);
-  }
-}
- alength = a.length -result.length;
-for(let j=0; j<alength;j++){
-  result.push(0)
-}
-console.log(result)
+// let num=15,fact=1;
+// for(let i=1;i<=num;i++) 
+//   fact*=i;
+// console.log(fact);
 
-let mat=[[1,2,3],[4,5,6]], t=[];
-for(let i=0;i<mat[0].length;i++){
-  t[i]=[];
-  for(let j=0;j<mat.length;j++) t[i][j]=mat[j][i];
-}
-console.log(t);
+// let a =0;
+// let b =1;
+// let n = 10;
 
-let arr = [1,2,3]
+// for(let i=1;i<=n;i++){
+//  [a+b]= [b,a+b]
+
+// }
+
+// shift zero to end
 
 
+// let a = [1,2,3,0,11,5,0,5];
+// let result = [];
+// let alength = 0;
+// for(let i=0; i<a.length;i++){
+//   if(a[i]!== 0){
+//     result.push(a[i]);
+//   }
+// }
+//  alength = a.length -result.length;
+// for(let j=0; j<alength;j++){
+//   result.push(0)
+// }
+// console.log(result)
 
-for(let i =0; i<arr.length; i++)
-{
-  let newar= [];
-  for(let j=i;j<arr.length;j++){
-    newar.push(arr[i])
-    console.log(newar)
-  }
+// let mat=[[1,2,3],[4,5,6]], t=[];
+// for(let i=0;i<mat[0].length;i++){
+//   t[i]=[];
+//   for(let j=0;j<mat.length;j++) t[i][j]=mat[j][i];
+// }
+// console.log(t);
 
-}
-
-let n =5;
+// let arr = [1,2,3]
 
 
-for(let i=1;i<=n;i++  ){
-  let row="";
-  for(let s=1;s<=n-i;s++  ){
-    row+=" ";
-  }
-  for(let j=1;j<=2*i-1;j++){
-    row+="*";
-  }
-  console.log(row)
-}
-for(let i=n-1;i>=1;i--){
-  let row="";
-  for(let s =1;s<=n-i;s++){
-    row+=" ";
-  }
-  for(let j=1;j<=2*i-1;j++){
-    row+="*";
-  }
-  console.log(row)
-}
-let n=5;
-for(let i=1;i<=n;i++){
-  let row="";
 
-  for(let s=1;s<=n-i;s++){
-    row+=" ";
-  }
-  for(j=1;j<=2*i-1;j++){
-    row+="*"
-  }
-  console.log(row)
-}
-for(let i =n;i>=1;i--){
-  let row="";
-  for(let s=1 ; s<=n-i;s++){
-    row+=" ";
-  }
-  for(let j=1;j<=2*i-1;j++){
-    row+="*";
-  }
-  console.log(row)
-}
+// for(let i =0; i<arr.length; i++)
+// {
+//   let newar= [];
+//   for(let j=i;j<arr.length;j++){
+//     newar.push(arr[i])
+//     console.log(newar)
+//   }
 
-for (let i = 1; i <= 10; i++) {
-  for (let j = 1; j <= 10; j++) {
-    document.write(i * j + "&nbsp;&nbsp;");
-  }
-  document.write("<br>");
-}
-let btn = document.getElementById("bt")
-btn.addEventListener("click", function() {
-    console.log("btn clicked");
-});
-let input = document.getElementById("inp");
-    input.addEventListener("keydown",function(x){
-        if(x.key == 'Enter'){
-            alert("enter")
-        }
-    });
+// }
 
-    let grand = document.getElementById("grand")
-    grand.addEventListener("click",function(){
-        alert("grand clicked")
-    })
+// let n =5;
 
-    let parent = document.getElementById("parent")
-    parent.addEventListener("click",function(){
+
+// for(let i=1;i<=n;i++  ){
+//   let row="";
+//   for(let s=1;s<=n-i;s++  ){
+//     row+=" ";
+//   }
+//   for(let j=1;j<=2*i-1;j++){
+//     row+="*";
+//   }
+//   console.log(row)
+// }
+// for(let i=n-1;i>=1;i--){
+//   let row="";
+//   for(let s =1;s<=n-i;s++){
+//     row+=" ";
+//   }
+//   for(let j=1;j<=2*i-1;j++){
+//     row+="*";
+//   }
+//   console.log(row)
+// }
+// let n=5;
+// for(let i=1;i<=n;i++){
+//   let row="";
+
+//   for(let s=1;s<=n-i;s++){
+//     row+=" ";
+//   }
+//   for(j=1;j<=2*i-1;j++){
+//     row+="*"
+//   }
+//   console.log(row)
+// }
+// for(let i =n;i>=1;i--){
+//   let row="";
+//   for(let s=1 ; s<=n-i;s++){
+//     row+=" ";
+//   }
+//   for(let j=1;j<=2*i-1;j++){
+//     row+="*";
+//   }
+//   console.log(row)
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     document.write(i * j + "&nbsp;&nbsp;");
+//   }
+//   document.write("<br>");
+// }
+// let btn = document.getElementById("bt")
+// btn.addEventListener("click", function() {
+//     console.log("btn clicked");
+// });
+// let input = document.getElementById("inp");
+//     input.addEventListener("keydown",function(x){
+//         if(x.key == 'Enter'){
+//             alert("enter")
+//         }
+//     });
+
+//     let grand = document.getElementById("grand")
+//     grand.addEventListener("click",function(){
+//         alert("grand clicked")
+//     })
+
+//     let parent = document.getElementById("parent")
+//     parent.addEventListener("click",function(){
         
-        alert("parent clicked")
-    })
-    let child = document.getElementById("child")
-    child.addEventListener("click",function(e){
-        alert("child clicked")
-        e.stopPropagation()
-    })
+//         alert("parent clicked")
+//     })
+//     let child = document.getElementById("child")
+//     child.addEventListener("click",function(e){
+//         alert("child clicked")
+//         e.stopPropagation()
+//     })
 
 
-    let list = document.getElementById("list")
-    let btn = document.getElementById("btn")
+//     let list = document.getElementById("list")
+//     let btn = document.getElementById("btn")
 
-    btn.addEventListener("click",function(){
-        let li = document.createElement("li")
-        li.innerText="hello"
-        list.append(li);
-    })
+//     btn.addEventListener("click",function(){
+//         let li = document.createElement("li")
+//         li.innerText="hello"
+//         list.append(li);
+//     })
 
-    list.addEventListener("click",(e)=>{
-        if(e.target.tagName === "LI")
-        {
-            alert("adsf")
-        }
-    })
-
-
+//     list.addEventListener("click",(e)=>{
+//         if(e.target.tagName === "LI")
+//         {
+//             alert("adsf")
+//         }
+//     })
 
 
- let inp = document.getElementById("inp");
-  let para = document.getElementById("para");
 
-  // Input event listener
-  inp.addEventListener("input", function() {
-    para.textContent = "sadf"+inp.value;
-  });
 
-  inp.addEventListener("input", function(){
-    inp.classList.add("highlight")
-  })
+//  let inp = document.getElementById("inp");
+//   let para = document.getElementById("para");
 
-  // 3️⃣ Highlight on focus, remove on blur
-  inp.addEventListener("focus", function() {
-    inp.classList.add("highlight");
-  });
+//   // Input event listener
+//   inp.addEventListener("input", function() {
+//     para.textContent = "sadf"+inp.value;
+//   });
 
-  inp.addEventListener("blur", function() {
-    inp.classList.remove("highlight");
-  });
+//   inp.addEventListener("input", function(){
+//     inp.classList.add("highlight")
+//   })
+
+//   // 3️⃣ Highlight on focus, remove on blur
+//   inp.addEventListener("focus", function() {
+//     inp.classList.add("highlight");
+//   });
+
+//   inp.addEventListener("blur", function() {
+//     inp.classList.remove("highlight");
+//   });
   
   
 
-let form = document.getElementById("myForm");
-let nameInput = document.getElementById("name");
-let emailInput = document.getElementById("email");
-let emailError = document.getElementById("emailError");
+// let form = document.getElementById("myForm");
+// let nameInput = document.getElementById("name");
+// let emailInput = document.getElementById("email");
+// let emailError = document.getElementById("emailError");
 
-let form = document.getElementById("myForm")
-let nameInput = document.getElementById("name")
-let emailInput = document.getElementById("email")
-let btnSubmit = document.getElementById("button")
+// let form = document.getElementById("myForm")
+// let nameInput = document.getElementById("name")
+// let emailInput = document.getElementById("email")
+// let btnSubmit = document.getElementById("button")
 
 
-form.addEventListener("submit",function(e){
-  e.preventDefault();
+// form.addEventListener("submit",function(e){
+//   e.preventDefault();
 
-  let isValid = true;
+//   let isValid = true;
 
-  if(nameInput.value.trim() === "" ){
-    nameInput.style.border= "2px solid red";
-    isValid = false;
-  }
-  else{
-    nameInput.style.border = "2px solid green"
-  }
+//   if(nameInput.value.trim() === "" ){
+//     nameInput.style.border= "2px solid red";
+//     isValid = false;
+//   }
+//   else{
+//     nameInput.style.border = "2px solid green"
+//   }
 
-  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(emailInput.value)) {
-    emailInput.style.border = "2px solid red"
-    emailError.innerText = "invalid email";
-    isValid=false
+//   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   if (!emailPattern.test(emailInput.value)) {
+//     emailInput.style.border = "2px solid red"
+//     emailError.innerText = "invalid email";
+//     isValid=false
 
-  }
-  else{
-    emailInput.style.border = "2px solid green"
-    emailInput.innerText="";
-  }
+//   }
+//   else{
+//     emailInput.style.border = "2px solid green"
+//     emailInput.innerText="";
+//   }
 
-  if(isValid){
-    alert("form submited")
-  }
+//   if(isValid){
+//     alert("form submited")
+//   }
 
-})
+// })
 
-form.addEventListener("submit", function(e) {
-  e.preventDefault(); // form submit rok do
+// form.addEventListener("submit", function(e) {
+//   e.preventDefault(); // form submit rok do
 
-  let isValid = true;
+//   let isValid = true;
 
-  // Name validation
-  if (nameInput.value.trim() === "") {
-    nameInput.style.border = "2px solid red";
-    isValid = false;
-  } else {
-    nameInput.style.border = "2px solid green";
-  }
+//   // Name validation
+//   if (nameInput.value.trim() === "") {
+//     nameInput.style.border = "2px solid red";
+//     isValid = false;
+//   } else {
+//     nameInput.style.border = "2px solid green";
+//   }
 
-  // Email validation
-  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   // Email validation
+//   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (!emailPattern.test(emailInput.value)) {
-    emailError.innerText = "Invalid email address";
-    emailInput.style.border = "2px solid red";
-    isValid = false;
-  } else {
-    emailError.innerText = "";
-    emailInput.style.border = "2px solid green";
-  }
+//   if (!emailPattern.test(emailInput.value)) {
+//     emailError.innerText = "Invalid email address";
+//     emailInput.style.border = "2px solid red";
+//     isValid = false;
+//   } else {
+//     emailError.innerText = "";
+//     emailInput.style.border = "2px solid green";
+//   }
 
-  // Final submit
-  if (isValid) {
-    alert("Form Submitted Successfully ✅");
-    form.reset();
-  }
-});
+//   // Final submit
+//   if (isValid) {
+//     alert("Form Submitted Successfully ✅");
+//     form.reset();
+//   }
+// });
+
+  // let form = document.getElementById("form")
+  // let input = document.getElementById("username")
+  // let count =  document.getElementById("count")
+  // let btnSubmit = document.getElementById("btnSubmit")
+  // let btnreset = document.getElementById("reset")
+  // let msg = document.getElementById("msg")
+
+  //   input.addEventListener("focus",function(){
+  //     input.style.border = "2px solid green"
+  //   })
+
+  //   input.addEventListener("blur",function(){
+  //     input.style.border = "2px solid red"
+  //   })
+  //   input.addEventListener("input",function(){
+  //     count.innerText = input.value.length+"characters";
+  //   })
+
+  //   form.addEventListener("submit",function(e){
+  //     e.preventDefault();
+
+      
+  //       if(input.value.trim()=== ""){
+  //         msg.style.border= "2px solid red "
+  //         msg.innerText="pLEASE INTER TEXT"
+        
+  //       }
+        
+  //       else{
+  //         msg.innerText="submited"
+  //       }
+  //     })
+
+  //     form.addEventListener("reset",function(){
+  //       msg.innerText=""
+  //       count.innerText= "0 Count"
+
+
+  //     })
+    
+
